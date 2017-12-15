d3d90ec9193ef26de68da4efce9de9058afa9814
unknown <kramera3@gmail.com>
2017-12-15 16:38:49 -0500

    People added

--- js/hugo-academic.js
+++ js/hugo-academic.js
@@ -263,6 +263,32 @@
       });
     });
 
+    // Filter people.
+    $('.people-container').each(function(index, container) {
+      let $container = $(container);
+      let $section = $container.closest('section');
+      let layout = 'masonry';
+      if ($section.find('.isotope').hasClass('js-layout-row')) {
+        layout = 'fitRows';
+      }
+
+      $container.imagesLoaded(function() {
+        // Initialize Isotope after all images have loaded.
+        $container.isotope({
+          itemSelector: '.isotope-item',
+          layoutMode: layout,
+          filter: $section.find('.default-people-filter').text()
+        });
+        // Filter items when filter link is clicked.
+        $section.find('.people-filters a').click(function() {
+          let selector = $(this).attr('data-filter');
+          $container.isotope({filter: selector});
+          $(this).removeClass('active').addClass('active').siblings().removeClass('active all');
+          return false;
+        });
+      });
+    });
+
     // Enable publication filter for publication index page.
     if ($('.pub-filters-select')) {
       filter_publications();
